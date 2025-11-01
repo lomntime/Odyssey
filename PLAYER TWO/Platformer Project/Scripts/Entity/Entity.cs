1b628e35d2460efbba51b9befe2b5e0085b8770e
lomntime <qiaoyiyang486@gmail.com>
2025-11-01 17:49:39 +0800

    状态机初始化相关

--- PLAYER TWO/Platformer Project/Scripts/Entity/Entity.cs
+++ PLAYER TWO/Platformer Project/Scripts/Entity/Entity.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
 
+/// <summary>
+/// 
+/// </summary>
 public abstract class EntityBase : MonoBehaviour
 {
 }
 
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public abstract class Entity<T> : EntityBase where T : Entity<T>
 {
 }
--- PLAYER TWO/Platformer Project/Scripts/Player/PlayerStateManager.cs
+++ PLAYER TWO/Platformer Project/Scripts/Player/PlayerStateManager.cs
@@ -1,9 +1,28 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
+/// <summary>
+/// 玩家实体状态管理器
+/// </summary>
 [RequireComponent(typeof(Player))]
 public class PlayerStateManager : EntityStateManager<Player>
 {
 
+    #region 内部函数
+
+    /// <inheritdoc />
+    protected override List<EntityState<Player>> GetAllStates()
+    {
+        return PlayerState.CreateInsListFromStringArray(m_stateNames);
+    }
+
+    #endregion
+    
+    #region 字段
+    
     [ClassTypeName(typeof(PlayerState))]
     public string[] m_stateNames;
+    
+    #endregion
+
 }
