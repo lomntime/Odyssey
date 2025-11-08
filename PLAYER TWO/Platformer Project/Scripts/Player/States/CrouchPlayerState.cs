8872242e5f67e21fe19e1a0e0ee0b57e2ee9d51c
lomntime <qiaoyiyang486@gmail.com>
2025-11-08 22:04:15 +0800

    增加下蹲状态

---
+++ PLAYER TWO/Platformer Project/Scripts/Player/States/CrouchPlayerState.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 下蹲状态
+/// </summary>
+public class CrouchPlayerState : PlayerState
+{
+    protected override void OnEnter(Player entity)
+    {
+    }
+
+    protected override void OnExit(Player entity)
+    {
+    }
+
+    protected override void OnStep(Player entity)
+    {
+    }
+
+    protected override void OnContact(Player entity, Collider other)
+    {
+    }
+}
