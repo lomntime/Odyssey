477f58ed2f8723580a73882c25a906a297a560cc
lomntime <qiaoyiyang486@gmail.com>
2025-11-09 22:03:38 +0800

    爬行状态

---
+++ PLAYER TWO/Platformer Project/Scripts/Player/States/CrawlingPlayerState.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 爬行状态
+/// </summary>
+public class CrawlingPlayerState : PlayerState
+{
+    protected override void OnEnter(Player entity)
+    {
+    }
+
+    protected override void OnExit(Player entity)
+    {
+    }
+
+    protected override void OnStep(Player entity)
+    {
+    }
+
+    protected override void OnContact(Player entity, Collider other)
+    {
+    }
+}
